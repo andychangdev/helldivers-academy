f643d3a9cc922b11304fdbc54e14a18786279c6f
andychangdev <andychang.qld@gmail.com>
2024-05-30 23:12:04 +1000

    feat: Added function that changes colour of stratagem keys upon correct keypress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 import { useData } from "../contexts/dataContext.jsx";
+import soundfx from "../assets/beep.wav"
 
 export function HomePage() {
-  const {stratagems} = useData(); 
+  const { stratagems } = useData();
   const [randomStratagem, setRandomStratagem] = useState(null);
+  const [matchedKeys, setMatchedKeys] = useState([]);
+  const audio = new Audio(soundfx)
+  let count = 0;
 
   const arrowMap = {
     up: "\u2B06", 
@@ -14,10 +18,35 @@ export function HomePage() {
 
   useEffect(() => {
     if (stratagems && stratagems.length > 0) {
-      const randomIndex = Math.floor(Math.random() * 58);
+      const randomIndex = Math.floor(Math.random() * stratagems.length-1);
       setRandomStratagem(stratagems[randomIndex]);
     }
   }, [stratagems]);
+
+  useEffect(() => {
+    if (randomStratagem) {
+      document.addEventListener('keydown', customFunction);
+      return () => {
+        document.removeEventListener('keydown', customFunction);
+      };
+    }
+  }, [randomStratagem]);
+
+  function customFunction(event) {
+    if (
+      (event.key === "ArrowUp" && randomStratagem.keys[count] === "up") ||
+      (event.key === "ArrowDown" && randomStratagem.keys[count] === "down") ||
+      (event.key === "ArrowLeft" && randomStratagem.keys[count] === "left") ||
+      (event.key === "ArrowRight" && randomStratagem.keys[count] === "right")
+    ) {
+      setMatchedKeys((prev) => [...prev, count]);
+      count++;
+      audio.play();
+    } else {
+      setMatchedKeys([]);
+      count = 0;
+    }
+  }
 
   function renderStratagem() {
     if (!randomStratagem) return null;
@@ -31,7 +60,10 @@ export function HomePage() {
 
         <div className="arrow-container">
           {randomStratagem.keys.map((key, index) => (
-            <span key={index} className="arrow">
+            <span
+              key={index}
+              className={`arrow ${matchedKeys.includes(index+1) ? "matched" : ""}`}
+            >
               {arrowMap[key]}
             </span>
           ))}
