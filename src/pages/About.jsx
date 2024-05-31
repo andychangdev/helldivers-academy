79c2f7a6ed04407fd1baaf236e19352946066503
andychangdev <andychang.qld@gmail.com>
2024-06-01 05:03:18 +1000

    feat: Added content to About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,11 @@
 export function About () {
     return (
-        <h1>About</h1>
+        <section id="about">
+            <h1>ABOUT</h1>
+            <p>Hey there! I'm Andy. This project was developed as part of a 1 week hackathon at Coder Academy, where the aim was to create a ReactJS app showcasing various skills, including asynchronous JavaScript, React, state management (using hooks, reducer, and global state), local storage, and React routing.</p>
+
+            <p>Many thanks to everyone who contributed to HellHub Collective's <a href="https://github.com/hellhub-collective/api">API</a>, which enabled the effortless retrieval of data for Helldivers 2.</p>
+
+        </section>
     )
 }
