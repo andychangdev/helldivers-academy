bc6ef00dcfc8f981fee80a9cc39948f9bba8997e
andychangdev <andychang.qld@gmail.com>
2024-05-31 16:56:22 +1000

    fix: Updated briefing spacing

--- src/pages/Briefing.jsx
+++ src/pages/Briefing.jsx
@@ -4,7 +4,7 @@ import mouse from "../assets/mouse.png"
 export function Briefing() {
     return (
         <Link to={"/operation"} id="briefing">
-            <h2>BRIEFING</h2>
+            <h2 className="briefing__text">BRIEFING</h2>
             <h3>OPERATION ""TACTICAL THUNDER""</h3>
             <p> Welcome aboard the Super Destroyer, helldiver! You are now the lifeline of our brave Helldivers fighting on the front lines! Your mission is <span>crucial</span>: deploy stratagems with <span>precision</span> and <span>speed</span> to ensure the survival and victory of our forces on the ground! </p>
             <p>Our troops are in the heat of battle, facing hordes of alien <span>scum</span>. They depend on you to deliver the firepower, the supplies, and the reinforcements they need. Each request you fulfill brings us one step closer to safeguarding Super Earth and upholding the freedom of our <span>glorious nation!</span></p>
@@ -14,10 +14,6 @@ export function Briefing() {
                 <img className="mouse__icon" src={mouse}></img>
                 <p className="mouse__text">CONTINUE</p>
             </div>
-
-            
-
-            
         </Link>
 
     )
