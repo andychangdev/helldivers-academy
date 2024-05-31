36dcdef7ce09964197dc3999cc6fdc7890ca891f
andychangdev <andychang.qld@gmail.com>
2024-05-31 16:48:22 +1000

    feat: Updated briefing and alignment

--- src/pages/Briefing.jsx
+++ src/pages/Briefing.jsx
@@ -8,7 +8,7 @@ export function Briefing() {
             <h3>OPERATION ""TACTICAL THUNDER""</h3>
             <p> Welcome aboard the Super Destroyer, helldiver! You are now the lifeline of our brave Helldivers fighting on the front lines! Your mission is <span>crucial</span>: deploy stratagems with <span>precision</span> and <span>speed</span> to ensure the survival and victory of our forces on the ground! </p>
             <p>Our troops are in the heat of battle, facing hordes of alien <span>scum</span>. They depend on you to deliver the firepower, the supplies, and the reinforcements they need. Each request you fulfill brings us one step closer to safeguarding Super Earth and upholding the freedom of our <span>glorious nation!</span></p>
-            <p> Remember, a true Helldiver never falters, never hesitates. Show those alien invaders the might of our stratagems and the indomitable spirit of Super Earth! Deploy with pride, deploy with honor!</p>
+            <p> <span>Your objective is clear:</span> input the correct sequence of arrow keys to deploy the requested stratagems. Speed and accuracy are paramount. The lives of our soldiers and the fate of our mission rest in your capable hands. </p>
 
             <div className="mouse">
                 <img className="mouse__icon" src={mouse}></img>
